5a904f1888f4464a3619cf457b11e4e2832a3ca5
Herbert Gandia <hgandia@gmail.com>
2022-08-08 15:32:12 -0400

    HEADERDETAILS

---
+++ src/app/shared/HEADERDETAILS.js
@@ -0,0 +1,59 @@
+import churchLogo from '../assets/img/churchlogo1.png';
+
+export const HEADERDETAILS = [
+    {
+        id: 0,
+        img: churchLogo,
+        title: 'Iglesia Pentecostal Estrella de Jacob',
+        subTitle: 'La mas linda del Bronx'
+    },
+    {
+        id: 1,
+        img: churchLogo,
+        title: 'Sociedad de Caballeros',
+        subTitle: 'Procura con diligencia presentarte a Dios aprobado, como obreo que no tiene de qué avergonzarse, que usa bien la palabra de verdad.',
+        verse: '~ 2 Timoteo 2:15'
+    },
+    {
+        id: 2,
+        img: churchLogo,
+        title: 'Sociedad de Damas',
+        subTitle: 'Engañosa es la gracia, y vana la hermosura; la mujer que teme a Jehova, esa sera alabada.',
+        verse: '~ Proverbios 31:30'
+    },
+    {
+        id: 3,
+        img: churchLogo,
+        title: 'Sociedad de Jovenes',
+        subTitle: 'Porque tú, oh Señor Jehová, eres mi esperenza, seguridad mia desde mi juventud.',
+        verse: '~ Salmos 71:5'
+    },
+    {
+        id: 4,
+        img: churchLogo,
+        title: 'Sociedad de Niños',
+        subTitle: 'El Texto aureo de los niños estara aqui en este espacio.',
+        verse: '~ Jericob 3:15'
+    },
+    {
+        id: 5,
+        img: churchLogo,
+        title: 'Eventos'
+    },
+    {
+        id: 6,
+        img: churchLogo,
+        title: 'Calendario de Eventos del Mes'
+    },
+    {
+        id: 7,
+        img: churchLogo,
+        title: 'Quienes Somos?'
+    },
+    {
+        id: 8,
+        img: churchLogo,
+        title: 'Contactenos',
+    }
+
+]
