0acdfd3a531c68d9457f5fe2d8f686928403038f
Herbert Gandia <hgandia@gmail.com>
2023-05-18 16:28:45 -0400

    visitorSlice

--- src/app/shared/FLYERS.js
+++ src/app/shared/FLYERS.js
@@ -1,13 +1,14 @@
 import flyerOne from '..//assets/img/campanaJovenes.jpg';
 import flyerTwo from '../assets/img/campanaDeCaballeros.jpg';
 import flyerThree from '../assets/img/salidaIsrael.jpg';
+import damasCampana from '../assets/img/damasCampana.jpg';
 
 export const FLYERS = [
     {
         id: 0,
-        image: flyerOne,
-        title: 'Campaña de Jovenes',
-        description: 'Bienvenidos a nuestra Campaña de Jovenes.  Unase a nosotros para adorar al Señor!'
+        image: damasCampana,
+        title: 'Campaña de Damas',
+        description: 'Bienvenidos a nuestra Campaña de Damas.  Junio 1 y 2.  Unase a nosotros para adorar al Señor!'
     },
     {
         id: 1,
@@ -26,35 +27,5 @@ export const FLYERS = [
         image: flyerOne,
         title: 'Campaña de Jovenes',
         description: 'Bienvenidos a nuestra Campaña de Jovenes.  Unase a nosotros para adorar al Señor!'
-    },
-    {
-        id: 4,
-        image: flyerTwo,
-        title: 'Campaña de Caballeros',
-        description: 'Bienvenidos a nuestra Campaña de Caballeros.  Unase a nosotros para adorar al Señor!'
-    },
-    {
-        id: 5,
-        image: flyerThree,
-        title: 'Salida a Israel!',
-        description: 'Unase a nosotros para caminar donde nuestro Señor Jesucristo camino.'
-    },
-    {
-        id: 6,
-        image: flyerOne,
-        title: 'Campaña de Jovenes',
-        description: 'Bienvenidos a nuestra Campaña de Jovenes.  Unase a nosotros para adorar al Señor!'
-    },
-    {
-        id: 7,
-        image: flyerTwo,
-        title: 'Campaña de Caballeros',
-        description: 'Bienvenidos a nuestra Campaña de Caballeros.  Unase a nosotros para adorar al Señor!'
-    },
-    {
-        id: 8,
-        image: flyerThree,
-        title: 'Salida a Israel!',
-        description: 'Unase a nosotros para caminar donde nuestro Señor Jesucristo camino.'
     }
 ]
--- src/features/visitors/visitorSlice.js
+++ src/features/visitors/visitorSlice.js
@@ -1,3 +1,46 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import { baseUrl } from '../../app/shared/baseUrl';
 
+
+export const postVisitor = createAsyncThunk(
+    'visitors/postVisitor',
+    async (petition, { dispatch }) => {
+        const response = await fetch(baseUrl + 'visitors/', {
+                                        method: 'POST',
+                                        headers: {
+                                                    'Content-Type': 'application/json'
+                                                 },
+                                                credentials: 'same-origin',
+                                        body: JSON.stringify(petition)
+            });
+
+            if (!response.ok) {
+                return Promise.reject(response.status);
+            }
+            const data = await response.json();
+            dispatch(petition);
+    }
+);
+
+const initialState = {
+    isLoading: true,
+    errMsg: ''
+};
+
+const visitorSlice = createSlice({
+    name: 'visitors',
+    initialState,
+    reducers:{},
+    extraReducers: {
+        [postVisitor.pending]: state => {
+            state.isLoading = true;
+        },
+        [postVisitor.rejected]: (state, action) => {
+            alert(
+                'Tu peticion no has sido sometida\nError: ' +
+                (action.error ? action.error.message : 'Sometimiento ha fallado')
+            );
+        }
+    }
+
+});
