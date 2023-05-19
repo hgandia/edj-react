4a132cbde97b0b6b843eb069ec52961e87567132
Herbert Gandia <hgandia@gmail.com>
2023-05-19 14:42:10 -0400

    Created Loading and Error

---
+++ src/components/Loading.js
@@ -0,0 +1,11 @@
+import { Col } from 'reactstrap';
+
+const Loading = () => {
+    return (
+        <Col>
+            <i class="fa-thin fa-star-christmas fa-spin" style="color: #1a0566;"></i>
+            <p style={{ fontFamily: 'papyrus' }}>Loading</p>
+        </Col>
+    );
+};
+export default Loading;
