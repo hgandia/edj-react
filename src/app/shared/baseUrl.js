97356cc15bc51fcdc697d6c72123238e0b7685e0
Herbert Gandia <hgandia@gmail.com>
2023-06-09 10:29:51 -0400

    Updated baseUrl

--- src/app/shared/baseUrl.js
+++ src/app/shared/baseUrl.js
@@ -1,3 +1,4 @@
 /*'http://localhost:3001/'
 Is the local host used to simulate a server locally using json-server.*/
-export const baseUrl = 'http://localhost:8080/';
+//export const baseUrl = 'http://localhost:8080/';
+export const baseUrl = 'https://us-central1-edjserver.cloudfunctions.net/edjServer/';
