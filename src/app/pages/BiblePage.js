26994bccc0bab05b2cce6aeb75c06e4e123c464b
Herbert Gandia <hgandia@gmail.com>
2024-01-24 10:37:08 -0500

    Interactive Bible - Displayed Books Sorted

--- src/app/pages/BiblePage.js
+++ src/app/pages/BiblePage.js
@@ -24,25 +24,38 @@ const BiblePage = () => {
         if(selectedBook && chapter){
             dispatch(fetchBibleBookChapter( {selectedBook, chapter} ));
         }
-        
     }, [dispatch, selectedBook, chapter]);
-
 
     useEffect(() => {
         setOTBooks(bibleArray.filter(book => book.testament === 'OT'));
+        console.log('otBooks: ', otBooks);
+        
     }, [ bibleArray, clickedOT]);
 
     useEffect(() => {
         setNTBooks(bibleArray.filter(book => book.testament === 'NT'));
+        console.log('ntBooks: ', ntBooks);
+        
     }, [ bibleArray, clickedNT]);  
 
     useEffect(() => {
         setBookChapters(bibleArray.find(book => book.id === selectedBook)?.chapters || []);
     }, [bibleArray, selectedBook]);
 
-    useEffect(() => {
-       setChapterText(bibleArray.filter(chapter => chapter.cleanText));
-       //console.log('chapterText Array is: ', chapterText);
+    useEffect(() => { 
+       bibleArray.filter(chapter => chapter);
+       const subsetBibleArray = bibleArray.map( text => text.cleanText );
+        
+        console.log('bibleArray is: ', bibleArray);
+        console.log('subseBibleArray is: ', subsetBibleArray);
+       setChapterText(subsetBibleArray);
+       console.log('chapterText is: ', chapterText);
+       return(
+        () => {
+            setChapterText([]);
+        }
+    );
+       
     }, [bibleArray, chapter]);
 
     return(
@@ -50,10 +63,10 @@ const BiblePage = () => {
             <Row>
                 <Col style={{textAlign: 'center'}}>
                     <h1 style={{ color: 'darkblue'}}>Biblia Interactiva</h1>
-                    <Button color='warning' size='lg' onClick={() => setClickedOT(true)}>
+                    <Button style={{margin: '2rem'}} color='warning' size='lg' onClick={() => setClickedOT(true)}>
                         Antiguo Testamento
                     </Button>{' '}
-                    <Button color='warning' size='lg' onClick={() => setClickedNT(true)}>
+                    <Button style={{margin: '2rem'}} color='warning' size='lg' onClick={() => setClickedNT(true)}>
                         Nuevo Testamento
                     </Button>
                 </Col>
@@ -61,28 +74,36 @@ const BiblePage = () => {
             <Row style={{marginTop:'4rem'}}>
                 <Col style={{textAlign:'left', marginLeft:'-18rem'}}>
                     {
-                         clickedOT && otBooks.map(book => (
-                                <Button color='primary' key={book.id} style={{ margin: '4px' }} onClick={() => setSelectedBook(book.id)}>
-                                    {book.name}
-                                </Button> 
-                        ))
-                    }
+                         clickedOT && otBooks.sort((b1, b2) => {
+                                    if (b1.order < b2.order) return -1;
+                                    if (b1.order > b2.order) return 1;
+                                    return 0;
+                                }).map(book => (
+                                    <Button color='primary' key={book.order} style={{ margin: '4px' }} onClick={() => setSelectedBook(book.id)}>
+                                        {book.name}
+                                    </Button> 
+                            ))
+                        }
                 </Col>
-                <Col>
+                <Col style={{textAlign:'left', marginRight:'-18rem'}}>
                     {
-                        clickedNT && ntBooks.map(book => (
-                            <Button color='primary' key={book.id} style={{ margin: '4px' }} onClick={() => setSelectedBook(book.id)}>
-                                {book.name}
-                            </Button>
-                    ))}
+                        clickedNT && ntBooks.sort((b1, b2) => {
+                                    if (b1.order < b2.order) return -1;
+                                    if (b1.order > b2.order) return 1;
+                                    return 0;
+                                }).map(book => (
+                                    <Button color='primary' key={book.order} style={{ margin: '4px' }} onClick={() => setSelectedBook(book.id)}>
+                                        {book.name}
+                                    </Button>
+                            ))}
                 </Col>
             </Row>
             <Row>
                 <Col style={{textAlign:'center', marginLeft:'', marginTop: '6rem'}}>
                         {
-                            selectedBook && bookChapters.map(chapter => (
-                                    <Button color='success' key={chapter.id} style={{ margin: '4px' }} onClick={() => setChapter(chapter.chapter)}>
-                                        {chapter.chapter}
+                            bookChapters.map(ch => (
+                                    <Button color='success' key={ch.id} style={{ margin: '4px' }} onClick={() => setChapter(ch.chapter)}>
+                                        {ch.chapter}
                                     </Button>
                                 )
                             )
@@ -92,9 +113,9 @@ const BiblePage = () => {
             <Row>
                 <Col style={{textAlign:'left', marginLeft:'', marginTop: '6rem'}}>
                         {
-                             chapter && chapterText.map((text, idx) => (
-                                    <p color='success' key={idx}>
-                                        {text.cleanText}
+                              chapterText.map((text, idx) => (
+                                    <p key={idx}>
+                                        {text}
                                     </p>
                                 )
                             )
