1af0ddfe58c6b6dcd939e7d8b73bf91d8b1c5cc1
Herbert Gandia <hgandia@gmail.com>
2024-02-05 15:30:10 -0500

    State Variable SelectedBookName - Displayed Index

--- src/app/pages/BiblePage.js
+++ src/app/pages/BiblePage.js
@@ -12,6 +12,7 @@ const BiblePage = () => {
     const [otBooks, setOTBooks] = useState([]);
     const [ntBooks, setNTBooks] = useState([]);
     const [selectedBook, setSelectedBook] = useState('');
+    const [selectedBookName, setSelectedBookName] = useState('');
     const [bookChapters, setBookChapters] = useState([]);
     const [chapter, setChapter] = useState(null);
     const [chapterText, setChapterText] = useState([]);
@@ -48,44 +49,89 @@ const BiblePage = () => {
        setChapterText(newBibleArray.map( text => text.text ));  
     }, [ newBibleArray, chapter ]);
 
-    console.log('newBibleArray: ', newBibleArray);
-    console.log('chapterText: ', chapterText);
-
     return(
         <Container>
             <Row>
                 <Col style={{textAlign: 'center', marginTop: '3.5rem'}}>
                     <h1 style={{ color: 'darkblue', fontWeight: "bold", fontSize: '8rem'}}>Biblia Interactiva</h1>
-                    <Button style={{margin: '2rem'}} color='warning' size='lg' onClick={() => setClickedOT(true)}>
+                    <Button 
+                            size='lg' 
+                            onClick={() => setClickedOT(true)}
+                            style={{ 
+                                    margin: '4rem', 
+                                    backgroundColor: '#00008B', 
+                                    fontFamily: 'papyrus, sans-serif',
+                                    fontSize: '2rem',
+                                    fontWeight: 'bold',
+                                    padding: '2rem',
+                                    border: '1rem double white',
+                                    borderRadius: '5rem'
+                    }}>
                         Antiguo Testamento
                     </Button>{' '}
-                    <Button style={{margin: '2rem'}} color='warning' size='lg' onClick={() => setClickedNT(true)}>
+                    <Button 
+                            size='lg' 
+                            onClick={() => setClickedNT(true)}
+                            style={{ 
+                                margin: '4rem', 
+                                backgroundColor: '#00008B', 
+                                fontFamily: 'papyrus, sans-serif',
+                                fontSize: '2rem',
+                                fontWeight: 'bold',
+                                padding: '2rem',
+                                border: '1rem double white',
+                                borderRadius: '5rem'
+                    }}>
                         Nuevo Testamento
                     </Button>
                 </Col>
             </Row>
             <Row style={{marginTop:'4rem'}}>
-                <Col style={{textAlign:'left', marginLeft:'-18rem'}}>
+                <Col style={{textAlign:'left', marginLeft:'-10rem'}}>
                     {
                         clickedOT && otBooks.sort((b1, b2) => {
                                     if (b1.order < b2.order) return -1;
                                     if (b1.order > b2.order) return 1;
                                     return 0;
                                 }).map(book => (
-                                    <Button color='primary' key={book.order} style={{ margin: '4px' }} onClick={() => setSelectedBook(book.id)}>
+                                    <Button 
+                                            size='lg' 
+                                            key={book.order}
+                                            onClick={() => {setSelectedBookName(book.name); setSelectedBook(book.id)}}
+                                            style={{ 
+                                                    margin: '4px', 
+                                                    backgroundColor:'#9400D3', 
+                                                    fontFamily:'papyrus',
+                                                    fontSize: '25px',
+                                                    fontWeight: 'bold',
+                                                    border: '1rem double white',
+                                                    borderRadius: '5rem' 
+                                    }}>
                                         {book.name}
                                     </Button> 
                             ))
                     }
                 </Col>
-                <Col style={{textAlign:'left', marginRight:'-18rem'}}>
+                <Col style={{textAlign:'left', marginRight:'-10rem'}}>
                     {
                         clickedNT && ntBooks.sort((b1, b2) => {
                                     if (b1.order < b2.order) return -1;
                                     if (b1.order > b2.order) return 1;
                                     return 0;
                                 }).map(book => (
-                                    <Button color='primary' key={book.order} style={{ margin: '4px' }} onClick={() => setSelectedBook(book.id)}>
+                                    <Button 
+                                            size='lg'
+                                            key={book.order} 
+                                            onClick={() => {setSelectedBookName(book.name); setSelectedBook(book.id)}}
+                                            style={{ 
+                                                margin: '4px', 
+                                                backgroundColor:'#9400D3', 
+                                                fontFamily:'papyrus',
+                                                fontSize: '25px',
+                                                fontWeight: 'bold',
+                                                border: '1rem double white',
+                                                borderRadius: '5rem' 
+                                }}>
                                         {book.name}
                                     </Button>
                             ))}
@@ -95,7 +141,19 @@ const BiblePage = () => {
                 <Col style={{textAlign:'center', marginLeft:'', marginTop: '6rem'}}>
                         {
                             selectedBook && bookChapters.map(ch => (
-                                    <Button color='success' key={ch.id} style={{ margin: '4px' }} onClick={() => setChapter(ch.chapter)}>
+                                    <Button 
+                                            size='lg'
+                                            key={ch.id} 
+                                            onClick={() => setChapter(ch.chapter)}
+                                            style={{ 
+                                                margin: '4px', 
+                                                backgroundColor:'#8B0000', 
+                                                fontFamily:'papyrus',
+                                                fontSize: '25px',
+                                                fontWeight: 'bold',
+                                                border: '1rem double white',
+                                                borderRadius: '3rem' 
+                                }}>
                                         {ch.chapter}
                                     </Button>
                                 )
@@ -104,7 +162,12 @@ const BiblePage = () => {
                 </Col>
             </Row>
             <Row>
-                <Col style={{textAlign:'left', marginLeft:'', marginTop: '6rem'}}>
+                <Col style={{ marginTop: '8rem'}}>
+                        <h1>{selectedBookName}{' '}{chapter}</h1>
+                </Col>
+            </Row>
+            <Row>
+                <Col style={{textAlign:'left', marginLeft:'', marginTop: '2rem'}}>
                         {
                             chapter && chapterText.map((text, idx) => (  
                                     <div key={idx} dangerouslySetInnerHTML={{__html: text}}/>
