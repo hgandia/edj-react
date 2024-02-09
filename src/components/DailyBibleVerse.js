f352936290d16619382c97a126a37a50620bb7c6
Herbert Gandia <hgandia@gmail.com>
2024-02-09 14:56:16 -0500

    Personalized Daily Bible Verse - Part 1

---
+++ src/components/DailyBibleVerse.js
@@ -0,0 +1,10 @@
+import { fetchBibleBooks, fetchBibleBookChapter } from '../../features/bible/bibleSlice';
+import { useDispatch, useSelector } from 'react-redux';
+import { Container, Row, Col } from 'reactstrap';
+import { useState, useEffect } from 'react';
+
+const DailyBibleVerse = () => {
+
+}
+
+export default DailyBibleVerse;
