e64ae4f85bc2c05e1dcc4a2cd9c06152cc1529f1
Herbert Gandia <hgandia@gmail.com>
2024-02-29 11:32:27 -0500

    Phone link Correction 2

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -14,7 +14,7 @@ const Footer = () => {
                     <a className='btn btn-link btn-lg' href='mailto:la1raestrelladejacob@gmail.com' id='footerIcon'><i className='fa fa-envelope-o align-self-center' /></a>
                     <a className='btn btn-link' href='mailto:la1raestrelladejacob@gmail.com' id='contact'>E Mail</a><br /><br />
                     <a className='btn btn-link btn-lg' href='tel:+1-718-733-9714' id='footerIcon'><i className='fa fa-phone align-self-center' /></a>
-                    <a className='btn btn-link' href='tel:1+718-733-9714' id='contact'>Telefono</a><br />
+                    <a className='btn btn-link' href='tel:+1-718-733-9714' id='contact'>Telefono</a><br />
                 </Col>
                 <Col className='text-center'>
                     <h4 className='papyrus'>Siganos por:</h4><br />
